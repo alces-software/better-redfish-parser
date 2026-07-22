09018cc8f4423020f724e6b4bf6c25c511c8c9e1
Calum Murphy <calumthemurphy@gmail.com>
2026-07-22 10:29:55 +0100

    saving local changes

--- frontend/app/components/newAsset.jsx
+++ frontend/app/components/newAsset.jsx
@@ -4,9 +4,20 @@ import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
-export default function NewAsset() {
+export default function NewAsset
+() {
    const router = useRouter();
-   
+   const [racks, setRacks] = useState([])
+
+   useEffect(() => {
+      async function getRacks() {
+         const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/racks`);
+         const data = await res.json();
+         setRacks(data.body);
+      }
+      getRacks();
+   }, []);
+
    async function handleSubmit(event) {
       event.preventDefault();
 
@@ -37,78 +48,90 @@ export default function NewAsset() {
       }
 
       router.push(`/assets?id=${data.body.uuid}`);
-      
+
    }
 
 
 
    return (
-    <div>
-      <div className="flex items-center">
-         <h1 className="font-semibold text-4xl">New product</h1>
-         <Link
-            href="/"
-            className="ml-4 h-min w-min rounded-full border border-slate-400 bg-slate-900 p-2 transition hover:-translate-y-1"
-         >
-            Cancel
-         </Link>
+      <div>
+         <div className="flex items-center">
+            <h1 className="font-semibold text-4xl">New asset</h1>
+            <Link
+               href="/"
+               className="ml-4 h-min w-min rounded-full border border-slate-400 bg-slate-900 p-2 transition hover:-translate-y-1"
+            >
+               Cancel
+            </Link>
+         </div>
+         <br />
+         <hr />
+         <br />
+
+         <form onSubmit={handleSubmit} className="mx-auto flex w-min flex-col rounded-lg border border-slate-400 bg-slate-900 shadow-2xl drop-shadow-2xl">
+            <h2 className="mb-4 rounded-t-lg bg-slate-800 p-4 text-2xl">Asset details</h2>
+
+            <div className="pl-2">
+               <p className="p-1">Name</p>
+               <input name="name" type="text" className="m-1 h-9 w-58 rounded-lg border p-1 text-white" />
+            </div>
+
+            <div className="mt-2 pl-2">
+               <p className="p-1">UUID</p>
+               <input name="uuid" type="text" className="m-1 h-9 w-58 rounded-lg border p-1 text-white" />
+            </div>
+
+            <div className="mt-2 pl-2">
+               <p className="p-1">Rack</p>
+               <select
+                  name="rack"
+                
+                  className="m-1 rounded-lg  h-9 w-58 border p-1 text-white"
+               >
+                  <option value="">Select a rack</option>
+
+                  {racks.map((rack) => (
+                     <option key={rack._id} value={rack._id}>
+                        {rack.name}
+                     </option>
+                  ))}
+               </select>
+            </div>
+
+            <div className="mt-2 pl-2">
+               <p className="p-1">U position</p>
+               <input name="uPosition" type="text" className="m-1 h-9 w-58 rounded-lg border p-1 text-white" />
+            </div>
+
+            <div className="mt-2 pl-2">
+               <p className="p-1">Notes</p>
+               <input name="notes" type="text" className="m-1 rounded-lg h-9 w-58 border p-1 text-white" />
+            </div>
+
+            <div className="mt-2 pl-2">
+               <p className="p-1">Hardware Data</p>
+               <input
+                  name="hardwareData"
+                  type="file"
+                  accept="application/json,.json,.txt"
+                  className="m-1 cursor-pointer rounded-full border border-gray-600 bg-gray-600 p-2"
+               />
+            </div>
+
+            <div className="my-4 flex justify-end px-4">
+               <button
+
+                  type="submit"
+                  className="cursor-pointer rounded-full border border-blue-700 bg-blue-700 px-2 py-1 transition hover:-translate-y-1"
+               >
+                  Create Asset
+               </button>
+
+
+            </div>
+         </form>
+
       </div>
-        <br/>
-  <hr/>
-  <br/>
 
-  <form onSubmit={handleSubmit} className="mx-auto flex w-min flex-col rounded-lg border border-slate-400 bg-slate-900 shadow-2xl drop-shadow-2xl">
-   <h2 className="mb-4 rounded-t-lg bg-slate-800 p-4 text-2xl">Asset details</h2>
-
-   <div className="pl-2">
-      <p className="p-1">Name</p>
-      <input name="name" type="text" className="m-1 rounded-lg border p-1 text-white" />
-   </div>
-
-   <div className="mt-2 pl-2">
-      <p className="p-1">UUID</p>
-      <input name="uuid" type="text" className="m-1 rounded-lg border p-1 text-white" />
-   </div>
-
-   <div className="mt-2 pl-2">
-      <p className="p-1">Rack</p>
-      <input name="rack" type="text" className="m-1 rounded-lg border p-1 text-white" />
-   </div>
-
-   <div className="mt-2 pl-2">
-      <p className="p-1">U position</p>
-      <input name="uPosition" type="text" className="m-1 rounded-lg border p-1 text-white" />
-   </div>
-
-   <div className="mt-2 pl-2">
-      <p className="p-1">Notes</p>
-      <input name="notes" type="text" className="m-1 rounded-lg border p-1 text-white" />
-   </div>
-
-   <div className="mt-2 pl-2">
-      <p className="p-1">Hardware Data</p>
-      <input
-         name="hardwareData"
-         type="file"
-         accept="application/json,.json,.txt"
-         className="m-1 cursor-pointer rounded-full border border-gray-600 bg-gray-600 p-2"
-      />
-   </div>
-
-   <div className="my-4 flex justify-end px-4">
-      <button
-    
-         type="submit"
-         className="cursor-pointer rounded-full border border-blue-700 bg-blue-700 px-2 py-1 transition hover:-translate-y-1"
-      >
-         Edit Asset
-      </button>
-      
-
-   </div>
-</form>
-
-      </div>
-      
    );
 }
